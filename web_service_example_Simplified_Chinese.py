7c9032b0e1205c997aad3764bc6f0c478ac9f185
pony <5373119@163.com>
2024-03-17 18:31:22 +0800

    location5

--- web_service_example_Simplified_Chinese.py
+++ web_service_example_Simplified_Chinese.py
@@ -74,7 +74,7 @@ def detect_faces_in_image(file_stream):
     
     print(f"pony: face_locations is  {face_locations}")
         
-    print(f"pony: first.width is  {face_locations[0][2]-face_locations[0][0]},first.height is  {face_locations[0][1]-face_locations[0][3]}")
+    print(f"pony: first.width is  {face_locations[2]-face_locations[0]},first.height is  {face_locations[1]-face_locations[3]}")
 
     face_landmarks_list = face_recognition.face_landmarks(img,face_locations=face_locations)
     
